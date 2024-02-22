f41b66ab83e5035b642bad1e9829bba914b5c999
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-02-22 18:12:24 +0530

    Time: 454 ms (54.04%) | Memory: 17.3 MB (79.30%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -16,9 +16,9 @@ class Solution:
         ]
         distances = [[float("inf") for _ in range(N+1)] for _ in range(N+1)]
         distances[0][0] = 1
-        queue = deque([(1 , 0 , 0)])
-        while queue :
-            distance , x , y = queue.popleft()
+        heap = [(1 , 0 , 0)]
+        while heap :
+            distance , x , y = heapq.heappop(heap)
             if x == N and y == N :
                 min_distance = min(min_distance , distance)
                 continue
@@ -26,5 +26,5 @@ class Solution:
                 i , j = x+dx , y+dy
                 if i>=0 and i <= N and j>=0 and j <=N and grid[i][j] == 0 and distance+1 < distances[i][j] :
                     distances[i][j] = distance+1
-                    queue.append((distance+1 , i , j))
+                    heapq.heappush(heap , (distance+1 , i , j))
         return -1 if min_distance == float("inf") else min_distance
