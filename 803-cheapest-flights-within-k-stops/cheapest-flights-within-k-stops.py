53e28c9275ac8c0957017addf8333ad6fddba2c4
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-02-23 10:02:56 +0530

    Time: 86 ms (94.60%) | Memory: 18 MB (71.46%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,20 +1,22 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], source: int, destination: int, k: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], source : int, destination : int, k: int) -> int:
         adj_list = [[] for _ in range(n)]
-        for src , dst , cost in flights :
-            adj_list[src].append((dst , cost))
-        queue = deque([(0 , source , 0)])
-        min_distance = float("inf")
-        distances = [float("inf") for _ in range(n)]
-        while queue :
-            stops , root , cost = queue.popleft()
+        for start , end , price in flights :
+            adj_list[start].append((end,price))
+        costs = [float("inf") for _ in range(n)]
+        costs[source] = 0
+        heap = [(0 , 0 , source)]
+        min_cost = float("inf")
+        while heap :
+            stops , cost , root = heapq.heappop(heap)
             if root == destination :
-                min_distance = min(min_distance , cost)
+                min_cost = min(min_cost , cost)
+                continue
             if stops > k : 
                 continue
-            for node , node_distance in adj_list[root] :
-                new_distance = node_distance + cost
-                if stops <= k and new_distance < distances[node] :
-                    distances[node] = new_distance
-                    queue.append((stops+1 , node , new_distance))
-        return -1 if min_distance == float("inf") else min_distance
+            for node , node_cost in adj_list[root] :
+                new_cost = cost + node_cost
+                if stops <= k and new_cost < costs[node] :
+                    costs[node] = new_cost
+                    heapq.heappush(heap , (stops+1 , new_cost , node))
+        return -1 if min_cost == float("inf") else min_cost
