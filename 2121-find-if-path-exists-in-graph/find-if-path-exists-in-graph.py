1187463e398fc6907b43b7a0125f8e200f2f1291
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-02-26 12:58:52 +0530

    Time: 1544 ms (59.99%) | Memory: 107.2 MB (92.13%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,37 @@
+class DSU : 
+    def __init__(self , n , edges = []):
+        self.n = n
+        self.rank = [0 for _ in range(self.n)]
+        self.parent = [i for i in range(self.n)]
+        for u , v in edges :
+            self.unionByRank(u,v)
+        
+    def findParent(self , u ):
+        if self.parent[u] == u :
+            return u
+        parent = self.findParent(self.parent[u])
+        self.parent[u] = parent
+        return parent
+    
+    def unionByRank(self , u , v ):
+        parent_u = self.findParent(u)
+        parent_v = self.findParent(v)
+        rank_u = self.rank[parent_u]
+        rank_v = self.rank[parent_v]
+        if rank_u == rank_v : 
+            self.parent[parent_v] = self.findParent(parent_u)
+            self.parent[v] = self.findParent(parent_v)
+            self.rank[parent_u] += 1
+        elif rank_u > rank_v : 
+            self.parent[parent_v] = self.findParent(parent_u)
+            self.parent[v] = self.findParent(parent_v)
+            self.rank[parent_u] += 1
+        else : 
+            self.parent[parent_u] = self.findParent(parent_v)
+            self.parent[u] = self.findParent(parent_u)
+            self.rank[parent_v] += 1
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        dsu = DSU(n , edges)
+        return dsu.findParent(source) == dsu.findParent(destination)
