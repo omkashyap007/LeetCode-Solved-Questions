8c5e90ed0b2bd0189a263a599e451f2ca8cab1df
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-02-28 13:03:13 +0530

    Time: 183 ms (50.39%) | Memory: 20.4 MB (76.51%) - LeetSync

---
+++ 721-accounts-merge/accounts-merge.py
@@ -0,0 +1,52 @@
+class DSU:
+
+    def __init__(self , n):
+        self.parent = [i for i in range(n)]
+        self.size = [1 for _ in range(n)]
+        
+    def findParent(self , u):
+        if self.parent[u] == u :
+            return u
+        parent = self.findParent(self.parent[u])
+        self.parent[u] = parent
+        return parent
+    
+    def union(self , u , v):
+        parent_u = self.findParent(u)
+        parent_v = self.findParent(v)
+        
+        size_u = self.size[parent_u]
+        size_v = self.size[parent_v]
+        
+        if size_u < size_v : 
+            self.parent[parent_u] = self.findParent(parent_v)
+            self.size[parent_v] += self.size[parent_u]
+            self.parent[u] = self.findParent(parent_v)
+        else :
+            self.parent[parent_v] = self.findParent(parent_u)
+            self.size[parent_u] += self.size[parent_v]
+            self.parent[v] = self.findParent(parent_u)
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        n = len(accounts)
+        dsu = DSU(n)
+        hash_map = {}
+        for i in range(len(accounts)):
+            for j in range(1 ,  len(accounts[i])):
+                mail = accounts[i][j]
+                if mail in hash_map :
+                    dsu.union(i , hash_map[mail])
+                else : 
+                    hash_map[mail] = i
+        a = [[] for _ in range(n)]
+        for i in range(len(accounts)):
+            for j in range(1 , len(accounts[i])) :
+                mail = a[dsu.findParent(i)].append(accounts[i][j])
+        answer = []
+        for i in range(len(a)):
+            if a[i] :
+                l = [accounts[i][0]] + sorted(list(set(a[i])))
+                answer.append(l)
+        return answer
