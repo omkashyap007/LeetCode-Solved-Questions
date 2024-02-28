694e56360f820acf4665dbb1b55a0c8587a8b143
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-02-28 19:36:35 +0530

    Time: 2060 ms (64.22%) | Memory: 36.9 MB (33.22%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.py
+++ 854-making-a-large-island/making-a-large-island.py
@@ -29,22 +29,6 @@ class DSU :
 
 class Solution:
 
-    def bfs(self , i , j , grid , visited , area , dsu , dirs):
-        n = len(grid)
-        visited[i][j] = True
-        area = 0
-        queue = deque([(i,j)])
-        while queue : 
-            i ,  j = queue.popleft()
-            area += 1
-            for dx , dy in dirs :
-                x = i+dx
-                y = j+dy
-                if  x>=0 and x<n and y>=0 and y<n and grid[x][y] == 1 and not visited[x][y]:
-                    visited[x][y] = True
-                    queue.append((x,y))
-        return area
-
     def largestIsland(self, grid: List[List[int]]) -> int:
         n = len(grid)
         dsu = DSU(n*n)
@@ -59,18 +43,9 @@ class Solution:
                             adj  = x*n+y
                             dsu.union(adj , curr)
         visited = [[False for _ in range(n)] for _ in range(n)]
-        area = [0 for _ in range(n*n)]
         max_area = 0
-        dirs = [(-1,0) , (0,1) , (1,0) , (0,-1)]
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 1 and not visited[i][j]:
-                    the_area = self.bfs(i , j , grid , visited , area , dsu , dirs )
-                    parent = dsu.findParent(i*n+j)
-                    area[parent] = the_area
-                    max_area = max(max_area , the_area)
         print(dsu.size)
-        print(area)
+        dirs = [(-1,0) , (0,1) , (1,0) , (0,-1)]
         for i in range(n):
             for j in range(n):
                 if grid[i][j] == 1 :
@@ -85,7 +60,7 @@ class Solution:
                     x_y_num = x*n+y
                     the_parent =  dsu.findParent(x_y_num) 
                     if the_parent not in parents :
-                        curr_area += area[dsu.findParent(x_y_num)]
+                        curr_area += dsu.size[dsu.findParent(x_y_num)]
                         parents.append(the_parent)
                 max_area = max(max_area , curr_area + 1)    
-        return max_area
+        return n*n if max_area == 0 else max_area
