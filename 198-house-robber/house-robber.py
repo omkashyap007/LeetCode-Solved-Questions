a7370ce3fdb14cf5c2babc72778eb6d6424f2314
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-18 18:05:51 +0530

    Time: 39 ms (33.64%) | Memory: 16.6 MB (47.10%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,20 +1,9 @@
 class Solution:
-
-    def dfs(self , index , nums , dp) :
-        if dp[index] != -1 : 
-            return dp[index]
-        curr_step = nums[index] + self.dfs(index-2 , nums , dp)
-        prev_step = self.dfs(index-1 , nums , dp)
-        answer = max(curr_step , prev_step)
-        dp[index] = answer
-        return answer
-
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1 :
-            return nums[0]
-        dp = [-1 for _ in range(len(nums))]
-        dp[0] = nums[0]
-        dp[1] = max(nums[0]  , nums[1])
-        for i in range(2 , len(nums)):
-            dp[i] = max(dp[i-1] , dp[i-2] + nums[i])
-        return dp[len(nums)-1]
+        a = 0
+        b = 0
+        for i in range(len(nums)):
+            c = max(nums[i] + a , b)
+            a = b
+            b = c
+        return b
