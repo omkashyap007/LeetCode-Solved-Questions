435bce3056e7257c8b04c9890b9d0057f47ad1cc
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-19 18:59:19 +0530

    Time: 28 ms (92.84%) | Memory: 16.6 MB (43.55%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -13,4 +13,7 @@ class Solution:
         for i in range(m):
             dp[i][0] = 1
         dp[0][0] = 1
-        return self.dfs(m-1 ,  n-1 , dp)
+        for i in range(1 , m):
+            for j in range(1 , n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
