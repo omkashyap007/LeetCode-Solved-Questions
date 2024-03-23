1588640ef0163ed80b53e028fec8f6e3d3bbf371
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-23 14:32:04 +0530

    Time: 81 ms (24.41%) | Memory: 19.4 MB (12.08%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.py
+++ 382-linked-list-random-node/linked-list-random-node.py
@@ -4,21 +4,23 @@ class Solution:
 
     def __init__(self, head: Optional[ListNode]):
         self.head = head
-        self.length = 0
-        temp_head = self.head
-        while temp_head:
-            self.length += 1
-            temp_head = temp_head.next
+        self.BIG_VALUE = 10**9+7
 
     def getRandom(self) -> int:
-        random_index = random.randint(1 ,self.length)
+        random_index = random.randint(0 , self.BIG_VALUE)
         temp_head = self.head
-        curr_index = 1
+        curr_index = 0
         while temp_head :
             if curr_index == random_index :
                 return temp_head.val
             curr_index += 1
             temp_head = temp_head.next
-
-        
+        random_index %= curr_index
+        temp_head = self.head
+        curr_index = 0
+        while temp_head :
+            if curr_index == random_index :
+                return temp_head.val
+            curr_index +=1 
+            temp_head = temp_head.next
         
