d29be57111b89c789ae173e146c39149df7dcc6e
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-23 13:56:28 +0530

    Time: 49 ms (89.06%) | Memory: 16.7 MB (73.76%) - LeetSync

---
+++ 1580-shuffle-the-array/shuffle-the-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        arr = [-1 for _ in range(len(nums))]
+        i = 0
+        j = n 
+        for k in range(0 ,len(nums),2):
+            arr[k] = nums[i]
+            arr[k+1] = nums[j]
+            i += 1
+            j += 1
+        for i in range(len(nums)):
+            nums[i] = arr[i]
+        return nums
