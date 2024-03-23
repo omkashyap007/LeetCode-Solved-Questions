fd34cfd7b5d6bc2b98c09eaf6766ac7abefee2a7
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-23 18:41:13 +0530

    Time: 39 ms (32.16%) | Memory: 16.6 MB (17.56%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,12 +1,16 @@
 class Solution:
 
-    @cache
-    def dfs(self , i , j):
-        if i == 0 and i == 0:
+    def dfs(self , i , j , cache):
+        if (i,j) in cache : 
+            
+            return cache[(i,j)]
+        if i == 0 and j == 0:
             return 1
-        up = self.dfs(i-1 , j) if i-1 >=0 else 0
-        left = self.dfs(i,j-1) if j-1>=0 else 0
-        return up+ left
+        up = self.dfs(i-1 , j , cache) if i-1 >=0 else 0
+        left = self.dfs(i,j-1 , cache) if j-1>=0 else 0
+        cache[(i,j)] = up+left
+        return up+left
 
     def uniquePaths(self, m: int, n: int) -> int:
-        return self.dfs(m-1,n-1)
+        cache = {}
+        return self.dfs(m-1,n-1 , cache)
