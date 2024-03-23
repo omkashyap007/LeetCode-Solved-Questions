dad699c03f9645b55e214b2bfe08ea9ead631774
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-23 20:06:18 +0530

    Time: 121 ms (78.72%) | Memory: 17.5 MB (57.06%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,22 +1,31 @@
 class Solution:
 
-    def dfs(self , i , j , matrix , cache) :
+    def dfs(self , i , j , matrix , dp) :
         if i < 0 :
             return 0
-        if (i,j) in cache : 
-            return cache[(i,j)]
+        if dp[i][j] != -1 : 
+            return dp[i][j]
         value =  matrix[i][j] + min(
             [
-                self.dfs(i-1 , j-1 , matrix , cache) if j-1>=0 else float("inf") ,
-                self.dfs(i-1 , j , matrix , cache) ,
-                self.dfs(i-1 , j+1 , matrix , cache) if j+1< len(matrix) else float("inf")
+                self.dfs(i-1 , j-1 , matrix , dp) if j-1>=0 else float("inf") ,
+                self.dfs(i-1 , j , matrix , dp) ,
+                self.dfs(i-1 , j+1 , matrix , dp) if j+1< len(matrix) else float("inf")
             ]
         )
-        cache[(i,j)] = value
+        dp[i][j] = value
         return value
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         answer = float("inf")
-        cache = {}
-        for i in range(len(matrix)):
-            answer = min(self.dfs(len(matrix)-1 , i , matrix , cache ) , answer)
-        return answer
+        n = len(matrix)
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        dp[0] = matrix[0]
+        for i in range(1,n):
+            for j in range(n):
+                dp[i][j] = matrix[i][j] + min(
+                    [
+                        dp[i-1][j-1] if j-1>=0 else float("inf") ,
+                        dp[i-1][j] ,
+                        dp[i-1][j+1] if j+1< len(matrix) else float("inf")
+                    ]
+                )
+        return min(dp[n-1])
