9c1c80bf337f466a466c19c3f74aa99dc8d7f408
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-03-31 15:08:58 +0530

    Time: 553 ms (32.53%) | Memory: 31 MB (38.43%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -1,20 +1,9 @@
 class Solution:
-
-    def dfs(self , index , nums , dp , max_val):
-        if dp[index] != - 1 : 
-            return dp[index]
-        if index < 0 :
-            return float("-inf")
-        max_sum = max(self.dfs(index-1 , nums , dp , max_val )+ nums[index] , nums[index])
-        dp[index] = max_sum
-        max_val[0] = max(max_val[0] , max_sum)
-        return max_sum
-
     def maxSubArray(self, nums: List[int]) -> int:
-        dp = [-1 for _ in range(len(nums))]
-        dp[0] = nums[0]
+        prev = nums[0]
         max_value = nums[0]
         for i in range(1 , len(nums)):
-            dp[i] = max(dp[i-1] + nums[i] , nums[i])
-            max_value = max(dp[i] , max_value)
+            curr = max(prev + nums[i] , nums[i])
+            prev = curr
+            max_value = max(curr , max_value)
         return max_value
