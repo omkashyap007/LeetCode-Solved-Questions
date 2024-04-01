cd36c9cea1c8a6ead066a22bb4c92d0138806433
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-01 21:52:25 +0530

    Time: 49 ms (56.35%) | Memory: 16.8 MB (16.24%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,15 +1,13 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        first_map = {}
-        second_map = {}
-        for i in nums1 :
-            first_map[i] = first_map.get(i , 0)+1
-        for i in nums2 :
-            second_map[i] = second_map.get(i,0)+1
-        inter = set(nums1).intersection(set(nums2))
+        hash_map = {}
         answer = []
-        for i in inter :
-            count = min(first_map[i] , second_map[i])
-            for _ in range(count):
+        for i in nums1 : 
+            hash_map[i] = hash_map.get(i,0)+1
+        for i in nums2 :
+            if i in hash_map : 
                 answer.append(i)
+                hash_map[i] -= 1
+                if hash_map[i]  == 0 :
+                    del hash_map[i]
         return answer
