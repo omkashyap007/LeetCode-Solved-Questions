98eb704656750a73ed9ab4b69323b9f0868771b0
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-09 19:45:05 +0530

    Time: 29 ms (89.41%) | Memory: 16.5 MB (90.84%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,41 +1,33 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return Testing().kmpAlgorithm(haystack , needle)
-
-class Testing: 
-    def getLps(self , pattern) :
+    def strStr(self, string : str, pattern : str) -> int:
+        answer = []
         lps = [0 for _ in range(len(pattern))]
         prevLps = 0
         i = 1
         while i < len(pattern) :
-            if pattern[i] == pattern[prevLps] :
+            if pattern[i] == pattern[prevLps] : 
                 prevLps += 1
                 lps[i] = prevLps
                 i += 1
-            else : 
-                if prevLps == 0 :
+            else :
+                if prevLps == 0 : 
                     lps[i] = 0
                     i += 1
                 else :
                     prevLps = lps[prevLps-1]
-        return lps
-        
-    def kmpAlgorithm(self , string , pattern) :
-        lps = self.getLps(pattern) 
-        answer = []
         i = j = 0
         while i < len(string) and j < len(pattern) :
             if string[i] == pattern[j] :
                 i += 1
                 j += 1
                 if j == len(pattern) :
-                    return i-j
+                    answer.append(i-j)
                     j = lps[j-1]
             else :
                 if j == 0 :
                     i += 1
-                else : 
+                else :
                     j = lps[j-1]
+        if answer : 
+            return answer[0]
         return -1
-        return answer
-
