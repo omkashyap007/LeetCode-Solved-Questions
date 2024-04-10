e220f4f2b2d7eb768e39feb9766ccb528adaea69
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-10 11:32:19 +0530

    Time: 50 ms (28.88%) | Memory: 16.7 MB (80.95%) - LeetSync

--- 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.py
@@ -1,5 +1,15 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        queue = deque([i for i in range(len(deck))])
+        deck.sort()
+        result = [0 for _ in range(len(deck))]
+        for i in range(len(deck)):
+            index = queue.popleft()
+            result[index] = deck[i]
+            if queue :
+                queue.append(queue.popleft())
+        return result
+
         answer = [0 for _ in range(len(deck))]
         deck.sort()
         skip = False
