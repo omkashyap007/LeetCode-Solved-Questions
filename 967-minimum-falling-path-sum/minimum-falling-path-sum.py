1a4e9a896296128b31fd00c3bbb8cb88e469495d
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-11 19:59:09 +0530

    Time: 209 ms (5.05%) | Memory: 18.3 MB (23.08%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,31 +1,23 @@
 class Solution:
 
-    def dfs(self , i , j , matrix , dp) :
-        if i < 0 :
-            return 0
-        if dp[i][j] != -1 : 
-            return dp[i][j]
-        value =  matrix[i][j] + min(
-            [
-                self.dfs(i-1 , j-1 , matrix , dp) if j-1>=0 else float("inf") ,
-                self.dfs(i-1 , j , matrix , dp) ,
-                self.dfs(i-1 , j+1 , matrix , dp) if j+1< len(matrix) else float("inf")
-            ]
-        )
-        dp[i][j] = value
-        return value
+    def dfs(self , i , j , matrix , cache ):
+        if j < 0 or j >= len(matrix[0]) :
+            return float("inf")
+        if i == 0 :
+            return matrix[i][j]
+        if (i,j) in cache : 
+            return cache[(i,j)]
+        min_value = float("inf")
+        for k in range(j-1 , j+2) :
+            min_value = min(
+                self.dfs(i-1, k ,  matrix , cache), min_value
+            )
+        cache[(i,j)] = matrix[i][j] + min_value
+        return matrix[i][j] + min_value
+
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        answer = float("inf")
-        n = len(matrix)
-        dp = [[-1 for _ in range(n)] for _ in range(n)]
-        dp[0] = matrix[0]
-        for i in range(1,n):
-            for j in range(n):
-                dp[i][j] = matrix[i][j] + min(
-                    [
-                        dp[i-1][j-1] if j-1>=0 else float("inf") ,
-                        dp[i-1][j] ,
-                        dp[i-1][j+1] if j+1< len(matrix) else float("inf")
-                    ]
-                )
-        return min(dp[n-1])
+        max_value = float("inf")
+        cache = {}
+        for i in range(len(matrix[0])):
+            max_value = min( self.dfs(len(matrix)-1 , i , matrix , cache ) , max_value)
+        return max_value
