96899c9d9cadfd63a3a8549c53a3173f81a3aed8
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-11 21:46:44 +0530

    Time: 969 ms (52.19%) | Memory: 22.5 MB (74.11%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.py
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.py
@@ -1,13 +1,13 @@
 class Solution:
 
-    def dfs(self , r , c1  , c2 , ROWS , COLS , grid , cache) :
+    def dfs(self , r , c1  , c2 , ROWS , COLS , grid , dp) :
         if r == ROWS-1 :
             if c1 == c2 :
                 return grid[r][c1]
             else :
                 return grid[r][c2] + grid[r][c1]
-        if (r,c1,c2)  in cache : 
-            return cache[(r,c1,c2)]
+        if dp[r][c1][c2] != -1  :
+            return dp[r][c1][c2]
         if c1 == c2 :
             cc = grid[r][c2]
         else :
@@ -17,8 +17,8 @@ class Solution:
             for j in range(c2-1 , c2+2):
                 if i < 0 or i >= COLS or j < 0 or j >= COLS :
                     continue
-                max_value = max(self.dfs(r+1 , i , j , ROWS, COLS,  grid , cache) , max_value)
-        cache[(r,c1,c2)] = max_value + cc
+                max_value = max(self.dfs(r+1 , i , j , ROWS, COLS,  grid , dp) , max_value)
+        dp[r][c1][c2] = max_value + cc
         return max_value + cc
 
     def cherryPickup(self, grid: List[List[int]]) -> int:
@@ -26,5 +26,7 @@ class Solution:
         COLS = len(grid[0])
         c1 = 0
         c2 = COLS-1
-        cache = {}
-        return self.dfs(0 , c1 , c2 , ROWS , COLS ,  grid , cache)
+        dp = [
+            [[-1 for _ in range(COLS) ] for _ in range(COLS) ] for _ in range(ROWS)
+        ]
+        return self.dfs(0 , c1 , c2 , ROWS , COLS ,  grid , dp)
