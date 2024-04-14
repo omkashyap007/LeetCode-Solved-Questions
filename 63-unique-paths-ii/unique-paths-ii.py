e2311764b050a6afbba482adc29b45b18dfb8d4d
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-14 15:41:18 +0530

    Time: 41 ms (77.05%) | Memory: 16.6 MB (90.60%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,25 +1,41 @@
 class Solution:
 
-    def dfs(self , i , j , grid , dp) :
-        if dp[i][j] != -1 :
-            return dp[i][j]
-        top = 0
-        left = 0
-        if grid[i][j] == 1 :
-            dp[i][j] = 0
-            return 0
-        if i-1 >= 0 :
-            top = self.dfs(i-1 , j , grid , dp)
-        if j-1 >= 0 :
-            left = self.dfs(i , j-1,  grid , dp)
-        dp[i][j] = top+left
-        return top+left
+    # def dfs(self , i , j , grid , dp) :
+    #     if dp[i][j] != -1 :
+    #         return dp[i][j]
+    #     top = 0
+    #     left = 0
+    #     if grid[i][j] == 1 :
+    #         dp[i][j] = 0
+    #         return 0
+    #     if i-1 >= 0 :
+    #         top = self.dfs(i-1 , j , grid , dp)
+    #     if j-1 >= 0 :
+    #         left = self.dfs(i , j-1,  grid , dp)
+    #     dp[i][j] = top+left
+    #     return top+left
             
 
     def uniquePathsWithObstacles(self, grid : List[List[int]]) -> int:
         if grid[0][0] == 1 : 
             return 0
-        dp = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        dp = [
+            [0 for _ in range(len(grid[0]))] for _ in range(len(grid))
+        ]
         dp[0][0] = 1
-        self.dfs(len(grid)-1 , len(grid[0])-1 , grid ,dp)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])) :
+                if i == 0 and j == 0 :
+                    continue
+                if grid[i][j] == 1 :
+                    dp[i][j] = 0
+                else :
+                    top = 0
+                    left = 0
+                    if i-1>=0 :
+                        top = dp[i-1][j]
+                    if j-1>=0 :
+                        left = dp[i][j-1]
+                    dp[i][j] = top+left
+        print(dp)
         return dp[-1][-1]
