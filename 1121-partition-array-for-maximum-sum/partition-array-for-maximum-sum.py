205bf99c2d59bd3b6c116958fe1bc6c262d1f0dd
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-14 14:47:04 +0530

    Time: 359 ms (44.11%) | Memory: 16.8 MB (45.54%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -1,17 +1,25 @@
 class Solution:
 
-    def dfs(self , index ,  arr , k ,  dp) : 
-        if index in dp : 
+    def dfs(self , index , arr , dp , k):
+
+        if index < 0 :
+            return 0
+        if index == 0 : 
+            return arr[index]
+
+        if index in dp :
             return dp[index]
-        max_value = 0
-        result = 0
-        for i in range(index , min(len(arr) , (index+k))) :
-            max_value = max(max_value , arr[i])
-            next_partition_max = self.dfs(i+1 , arr , k , dp)
-            result = max( result , max_value*(i-index+1) + next_partition_max)
-        dp[index] = result
-        return result
+        
+        max_value = -1
+        max_sum = 0
+        for j in range(index , max(-1, index-k) , -1) :
+            max_value = max(max_value , arr[j])
+            max_sum = max(self.dfs(j-1 , arr , dp , k) + max_value * (index-j+1) , max_sum)
+        dp[index] = max_sum
+        return max_sum
 
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
-        dp = {len(arr):0}
-        return self.dfs(0 , arr , k ,  dp )
+        dp = {0:arr[0]}
+        self.dfs(len(arr)-1 , arr , dp , k)
+        print(dp)
+        return dp[len(arr)-1]
