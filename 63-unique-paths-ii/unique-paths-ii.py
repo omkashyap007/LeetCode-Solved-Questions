0f81b8f5c7f431e02416406f96bc3895c2c5cfd3
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-14 15:14:13 +0530

    Time: 44 ms (56.90%) | Memory: 16.6 MB (52.88%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,22 +1,25 @@
 class Solution:
 
     def dfs(self , i , j , grid , dp) :
-        if (i,j) in dp :
-            return dp[(i,j)]
+        if dp[i][j] != -1 :
+            return dp[i][j]
         top = 0
         left = 0
         if grid[i][j] == 1 :
+            dp[i][j] = 0
             return 0
         if i-1 >= 0 :
             top = self.dfs(i-1 , j , grid , dp)
         if j-1 >= 0 :
             left = self.dfs(i , j-1,  grid , dp)
-        dp[(i,j)] = top+left
+        dp[i][j] = top+left
         return top+left
             
 
     def uniquePathsWithObstacles(self, grid : List[List[int]]) -> int:
         if grid[0][0] == 1 : 
             return 0
-        dp = {(0,0) : 1}
-        return self.dfs(len(grid)-1 , len(grid[0])-1 , grid , dp)
+        dp = [[-1 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        dp[0][0] = 1
+        self.dfs(len(grid)-1 , len(grid[0])-1 , grid ,dp)
+        return dp[-1][-1]
