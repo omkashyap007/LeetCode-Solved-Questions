97263c0043cce273b06028a9abfec4ffcf7ab9c1
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-14 14:59:51 +0530

    Time: 44 ms (56.90%) | Memory: 16.7 MB (52.88%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,19 +1,22 @@
 class Solution:
-    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1 :
+
+    def dfs(self , i , j , grid , dp) :
+        if (i,j) in dp :
+            return dp[(i,j)]
+        top = 0
+        left = 0
+        if grid[i][j] == 1 :
             return 0
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        dp = [[-1 for _ in range(COLS)] for _ in range(ROWS)]
-        dp[0][0] = 1
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 1:
-                    dp[i][j] = 0
-                else :
-                    if i==0 and j == 0 :
-                        continue
-                    up = dp[i-1][j] if i-1>=0 else 0
-                    left = dp[i][j-1] if j-1>=0 else 0
-                    dp[i][j] = up+left
-        return dp[ROWS-1][COLS-1]
+        if i-1 >= 0 :
+            top = self.dfs(i-1 , j , grid , dp)
+        if j-1 >= 0 :
+            left = self.dfs(i , j-1,  grid , dp)
+        dp[(i,j)] = top+left
+        return top+left
+            
+
+    def uniquePathsWithObstacles(self, grid : List[List[int]]) -> int:
+        if grid[0][0] == 1 : 
+            return 0
+        dp = {(0,0) : 1}
+        return self.dfs(len(grid)-1 , len(grid[0])-1 , grid , dp)
