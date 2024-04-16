3b2d3fd78c039fc8027fe038c74649b39b210a41
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-16 16:03:10 +0530

    Time: 52 ms (21.75%) | Memory: 17.8 MB (73.75%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def dfs(self , root , height , val , depth) :
+        if not root  : return 
+        if height + 1 == depth :
+            node = TreeNode(val)
+            l = root.left
+            root.left = node
+            node.left = l
+            node = TreeNode(val)
+            r = root.right
+            node.right = r
+            root.right = node
+            return
+
+        else :
+            self.dfs(root.left , height+1 , val , depth)
+            self.dfs(root.right , height+1 , val , depth)
+
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if depth == 1:
+            node = TreeNode(val)
+            node.left = root
+            return node
+        self.dfs(root , 1 , val , depth)
+        return root
