da1331071cae8b81a9889f41f75132b6d46dd247
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-16 22:07:01 +0530

    Time: 45 ms (67.50%) | Memory: 17.7 MB (97.00%) - LeetSync

--- 623-add-one-row-to-tree/add-one-row-to-tree.py
+++ 623-add-one-row-to-tree/add-one-row-to-tree.py
@@ -1,17 +1,19 @@
 class Solution:
-
-    def dfs(self , root , height , val , depth) :
-        if not root  : return 
-        if height + 1 == depth :
-            node = TreeNode(val , left =  root.left)
-            root.left = node
-            node = TreeNode(val , right = root.right)
-            root.right = node
-            return
-        self.dfs(root.left , height+1 , val , depth)
-        self.dfs(root.right , height+1 , val , depth)
-
     def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
-        if depth == 1:return TreeNode(val , left  = root)
-        self.dfs(root , 1 , val , depth)
-        return root
+        if depth == 1  : return TreeNode(val , left = root)
+        a = root
+        queue = [root]
+        level = 1
+        for _ in range(depth-2) :
+            temp_queue = []
+            for node in queue :
+                if node.left :
+                    temp_queue.append(node.left)
+                if node.right : 
+                    temp_queue.append(node.right)
+            queue = temp_queue
+        for node in queue :
+            node.left = TreeNode(val , left = node.left) 
+            node.right = TreeNode(val , right = node.right)
+        return a
+                
