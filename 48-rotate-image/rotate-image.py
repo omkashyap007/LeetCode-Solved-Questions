b2e4cfa465d1917a2c6bfcad4fb9ef60ef1b1dd5
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-17 15:13:43 +0530

    Time: 48 ms (5.52%) | Memory: 16.5 MB (96.98%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -1,5 +1,13 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
+        grid = [[-1 for _ in range(len(matrix))] for _ in range(len(matrix))]
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix)):
+        #         grid[j][len(matrix)-i-1] = matrix[i][j]
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix)) :
+        #         matrix[i][j] = grid[i][j]
+        
         for i in range(len(matrix)):
             for j in range(i , len(matrix)) :
                 matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
@@ -10,4 +18,3 @@ class Solution:
                 matrix[i][l] , matrix[i][r] = matrix[i][r] , matrix[i][l]
                 l += 1
                 r -= 1
-        
