0c6fce631817e1fd66811aeea3de6392d0b6d452
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2024-04-20 15:23:03 +0530

    Time: 982 ms (31.50%) | Memory: 35.2 MB (33.00%) - LeetSync

--- 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
+++ 2103-find-all-groups-of-farmland/find-all-groups-of-farmland.py
@@ -9,6 +9,18 @@ class Solution:
         self.markVisited(i , j-1 , grid , visited)
         self.markVisited(i , j+1 , grid , visited)
 
+    def dfs(self , i , j , grid ,visited , group) :
+        if not ( 0 <= i <len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == 1 and not visited[i][j] ) :
+            return
+        visited[i][j] = True
+        group[2] = max(group[2] , i)
+        group[3] = max(group[3] , j)
+        self.dfs(i+1 , j , grid , visited ,group)
+        self.dfs(i-1 , j , grid , visited ,group)
+        self.dfs(i , j-1 , grid , visited ,group)
+        self.dfs(i , j+1 , grid , visited ,group)
+
+
     def findFarmland(self, grid : List[List[int]]) -> List[List[int]]:
         answer = []
         visited = [[False for _ in  range(len(grid[0]))] for _ in range(len(grid))]
@@ -16,14 +28,6 @@ class Solution:
             for j in range(len(grid[0])):
                 if grid[i][j] == 1 and not visited[i][j] :
                     group = [i , j , i , j]
-                    nj = j
-                    ni = i
-                    while ni < len(grid) and grid[ni][j] == 1 :
-                        ni += 1
-                    group[2] = ni-1
-                    while nj < len(grid[0]) and grid[i][nj] == 1 :
-                        nj += 1 
-                    group[3] = nj-1
-                    self.markVisited(i ,j , grid, visited)
+                    self.dfs(i , j, grid , visited , group)
                     answer.append(group)
         return answer
