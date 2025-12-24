c123f05ce22874329082897a73106980e3a8d00d
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2025-12-24 18:37:43 +0530

    Time: 31 ms (19.28%) | Memory: 19.2 MB (89.23%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,17 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        water_trapped = 0 
-        left = [-1 for _ in range(len(height))]
-        right = [-1 for _ in range(len(height))]
-        max_left = float("-inf")
-        max_right = float("-inf")
-        for i in range(1 , len(height)):
-            max_left = max(max_left , height[i-1])
-            left[i] = max_left
-        for j in range(len(height)-2 , -1 , -1):
-            max_right = max(max_right , height[j+1])
-            right[j] = max_right
-        for i in range(1 , len(height)-1) :
-            w = min(left[i] , right[i]) - height[i]
-            water_trapped += w if w > 0 else 0 
-        return water_trapped
+        right_max_array = [0 for _ in range(len(height))]
+        left_max = height[0]
+        right_max = height[-1]
+        for i in range(len(height)-1, -1, -1):
+            right_max = max(right_max, height[i])
+            right_max_array[i] = right_max
+        
+        water = 0
+        for i in range(len(height)):
+            left_max = max(left_max, height[i])
+            min_height = min(right_max_array[i], left_max)
+            _water = min_height - height[i]
+            water += _water if _water > 0 else 0
+        return water
