feffbaa6989adb7b6024ea9a3b04dfd2824efb47
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2025-12-26 14:17:30 +0530

    Time: 142 ms (34.25%) | Memory: 18.8 MB (45.93%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,20 +1,20 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n_count = 0
-        y_count = 0
-        suffix = [0 for _ in range(len(customers))]
-        for i in range(len(customers)-1, -1, -1):
-            e = customers[i]
-            val = y_count + (e == "Y")
-            suffix[i] = val
-            y_count += e == "Y"
+        y_count = sum([i == "Y" for i in customers])
+        # for i in range(len(customers)-1, -1, -1):
+        #     e = customers[i]
+        #     val = y_count + (e == "Y")
+        #     suffix[i] = val
+        #     y_count += e == "Y"
         answer = float("inf")
         index = None
         for i in range(len(customers)):
             e = customers[i]
-            p = n_count + (e == "Y")
+            p = n_count + (e == "Y")            
             n_count += e == "N"
-            s = suffix[i+1] if i+1 <= len(suffix)-1 else 0
+            y_count -= e == "Y"
+            s = y_count
             if p+s < answer:
                 answer = p+s
                 index = i
