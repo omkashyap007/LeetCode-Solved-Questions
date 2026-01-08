f633a74b1087c131eccd2f436f37fe7a7199bd3f
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2026-01-09 02:02:13 +0530

    Time: 527 ms (6.60%) | Memory: 53.5 MB (19.80%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.py
@@ -9,22 +9,14 @@ class Solution:
 
         take = a[i] * b[j]
         backtrack_value = self.findProduct(i-1, j-1, a, b, cache)
-        if backtrack_value != float("-inf"):
-            if take < 0 and backtrack_value < 0:
-                take = max(take, backtrack_value)
-            elif take < 0 and backtrack_value > 0:
-                take = backtrack_value
-            elif take > 0 and backtrack_value < 0:
-                take = take
-            else:
-                take += backtrack_value
+        take = max(take, backtrack_value, take+backtrack_value)
         
         not_take = self.findProduct(i-1, j, a, b, cache)
         not_take_both = self.findProduct(i, j-1, a, b, cache)
         max_value = max(take, not_take, not_take_both)
         cache[(i,j)] = max_value
         return max_value
-        
+
     def maxDotProduct(self, nums1: List[int], nums2: List[int]) -> int:
         cache = {}
         return self.findProduct(len(nums1)-1, len(nums2)-1, nums1, nums2, cache)
