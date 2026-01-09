e149e11cb4aa0f220c0e98a5614ba864c6f010cb
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2026-01-09 21:36:29 +0530

    Time: 62 ms (13.99%) | Memory: 23.4 MB (5.01%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,16 +7,23 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return root
-        
-        if root == p or root == q:
-            return root
-
-        left = self.lowestCommonAncestor(root.left, p, q)
-        right = self.lowestCommonAncestor(root.right, p, q)
-        
-        if left and right:
-            return root
-
-        return left or right
+        child_parent_map = {}
+        queue = [(root, None)]
+        while queue:
+            new_queue = []
+            for node, parent in queue:
+                child_parent_map[node] = parent
+                if node.left:
+                    new_queue.append((node.left, node))
+                if node.right:
+                    new_queue.append((node.right, node))
+            queue = new_queue
+        hash_set = set()
+        while p:
+            hash_set.add(p)
+            p = child_parent_map[p]
+        while q:
+            if q in hash_set:
+                return q
+            q = child_parent_map[q]
+            
