7f3a6fbc1352941d5d848630aa4dac310d12e9e3
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2026-01-11 01:22:47 +0530

    Time: 1386 ms (12.04%) | Memory: 187.1 MB (17.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,25 +1,21 @@
 class Solution:
 
-    def getMaxLength(self, i, j, text1, text2):
-        if i >= len(text1) or j >= len(text2):
+    def dfs(self, i, j,  l, m, cache):
+        if i >= len(l) or j >= len(m):
             return 0
-        elif text1[i] == text2[j]:
-            return 1 + self.getMaxLength(i+1, j+1, text1, text2)
-        else:
-            return max(
-                self.getMaxLength(i, j+1, text1, text2),
-                self.getMaxLength(i+1, j, text1,  text2)
-            )
+        
+        if (i,j) in cache:
+            return cache[(i, j)]
+
+        if l[i] == m[j]:
+            return 1 + self.dfs(i+1, j+1, l, m, cache)
+        a = self.dfs(i+1, j, l, m, cache)
+        b = self.dfs(i, j+1, l, m, cache)
+        answer =max(a, b)
+        cache[(i, j)] = answer
+        return answer
+
 
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        grid = [[0 for _ in range(len(text2))] for _ in range(len(text1))]
-        for i in range(len(text1)):
-            for j in range(len(text2)):
-                if text1[i] == text2[j]:
-                    top = grid[i-1][j-1] if i-1 >= 0 and j-1 >= 0 else 0
-                    grid[i][j] = 1 + top
-                else:
-                    top = grid[i-1][j] if i-1 >= 0 else 0
-                    left = grid[i][j-1] if j-1 >= 0 else 0
-                    grid[i][j] = max(top, left)
-        return grid[len(text1)-1][len(text2)-1]
+        cache = {}
+        return self.dfs(0, 0, text1, text2, cache)
