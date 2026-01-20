105f868e4c0346fb30a2b75f300e65471753ba4a
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2026-01-21 00:52:06 +0530

    Time: 764 ms (24.32%) | Memory: 26.3 MB (24.77%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -1,38 +1,43 @@
 class Solution:
-    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
-        m, n = len(mat), len(mat[0])
 
-        # build 2D prefix sum
-        ps = [[0]*(n+1) for _ in range(m+1)]
-        for i in range(m):
-            for j in range(n):
-                ps[i+1][j+1] = (
-                    mat[i][j]
-                    + ps[i][j+1]
-                    + ps[i+1][j]
-                    - ps[i][j]
-                )
+    def getSum(self, i, j, x, y, prefix):
+        _sum = (
+            (prefix[i][j])
+            - (prefix[x-1][j] if x-1 >= 0 else 0)
+            - (prefix[i][y-1] if y-1 >= 0 else 0)
+            + (prefix[x-1][y-1] if x-1 >= 0 and y-1 >= 0 else 0)
+        )
+        return _sum
 
-        def exists(k):
-            for i in range(m - k + 1):
-                for j in range(n - k + 1):
-                    s = (
-                        ps[i+k][j+k]
-                        - ps[i][j+k]
-                        - ps[i+k][j]
-                        + ps[i][j]
-                    )
-                    if s <= threshold:
-                        return True
-            return False
-
-        low, high, ans = 1, min(m, n), 0
+    def binarySearch(self, i, j, k, prefix, t):
+        answer = 0
+        low = 0
+        high = k
         while low <= high:
-            mid = (low + high) // 2
-            if exists(mid):
-                ans = mid
+            mid = low + (high-low)//2
+            _sum = self.getSum(i+mid, j+mid, i, j, prefix)
+            if _sum <= t:
+                answer = mid + 1
                 low = mid + 1
             else:
                 high = mid - 1
+        return answer
 
-        return ans
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        answer = 0
+        ROWS, COLS = len(mat), len(mat[0])
+        prefix = [[0 for _ in range(COLS)] for _ in range(ROWS)]
+        for i in range(ROWS):
+            for j in range(COLS):
+                a = prefix[i-1][j] if i-1 >=0 else 0
+                b = prefix[i][j-1] if j-1 >=0 else 0 
+                c = prefix[i-1][j-1] if i-1 >=0 and j-1 >=0 else 0
+                d = mat[i][j]
+                prefix[i][j] = a + b - c + d
+
+        for i in range(ROWS):
+            for j in range(COLS):
+                k = min(ROWS-i, COLS-j) - 1
+                _answer = self.binarySearch(i, j, k, prefix, threshold)
+                answer = max(answer, _answer)
+        return answer
