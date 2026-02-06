9e6e6574406f2e104d07b218fbc15f5e03432868
Om Kashyap <63098617+omkashyap007@users.noreply.github.com>
2026-02-07 02:06:33 +0530

    Time: 1111 ms (5.32%) | Memory: 34.9 MB (6.74%) - LeetSync

--- 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
+++ 3958-minimum-removals-to-balance-array/minimum-removals-to-balance-array.py
@@ -1,11 +1,32 @@
 class Solution:
+
+    def binarySearch(self, i, j, k, nums):
+        a = i
+        answer = i
+        while i<=j:
+            mid = (i+j)//2
+            if nums[mid] <= nums[a]*k:
+                i = mid+1
+                answer = mid
+            else:
+                j = mid-1
+        return answer
+
+
     def minRemoval(self, nums: List[int], k: int) -> int:
         nums.sort()
-        i = 0
-        j = 0
         max_len = 0
         for i in range(len(nums)):
-            while j < len(nums) and nums[j] <= nums[i]*k:
-                j += 1
-            max_len = max((j-i), max_len)
+            j = self.binarySearch(i, len(nums)-1, k, nums)
+            length = (j-i+1)
+            max_len = max(length, max_len)
         return len(nums) - max_len
+
+
+
+"""
+  mid
+1, 2, 6, 9
+
+
+"""
